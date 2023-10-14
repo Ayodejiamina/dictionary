2ba82b3d3f31af7f5ca9a36e58308c9fa3d5d0e9
Ayodeji amina <ayodjiamina.gmail.com>
2023-10-14 07:42:45 -0700

    did some adjustments to the design

---
+++ dom.js
@@ -0,0 +1,12 @@
+let body = document.body;
+let div1 = document.createElement("div")
+body.appendChild(div1);
+let h1_1 = document.createElement("h1")
+h1_1.innerHTML ="MY SELF"
+div1.appendChild(h1_1)
+let h2_1 = document.createElement('h2')
+h2_1.innerHTML ="My name is Ayodeji Aminat Ayomide"
+div1.appendChild(h2_1)
+let p1 = document.createElement("p")
+p1.innerHTML = "I am fair in complexion"
+div1.appendChild(p1)
---
+++ tag.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName('h1')[1].innerHTML= "she is a girl"
+document.querySelector(".classic").innerHTML = "Mr Omolewu Sunday his my Master"
